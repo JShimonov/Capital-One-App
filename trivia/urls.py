df4b8ee8e68f89146703a35e644d75aee1c7dfb4
JShimonov <jshimonov@gmail.com>
2019-10-24 20:24:01 -0400

    first push

---
+++ trivia/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='trivia-home'),
+    path('about/', views.about, name='trivia-about'),
+    path('categories/', views.categories, name='trivia-categories'),
+]
---
+++ trivia/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+import requests
+import re
+
+'''
+questions = [
+    {
+        "id":11510,
+        "title":"pair of dice, lost",
+        "clues_count":5
+    },
+    {
+        "id":11531,
+        "title":"mixed bag",
+        "clues_count":5
+    }
+]
+'''
+
+def home(request):
+    req = 'http://jservice.io/api/random?count=12'
+    response = requests.get(req)
+    trivia_set = response.json()
+    content = []
+    for trivia in trivia_set:
+        dict = { 'id': trivia['id'], 'question' : trivia['question'], 'answer' : trivia['answer'], 'category' : trivia['category']['title'] }
+        content.append(dict)
+    return render(request, 'trivia/home.html', {'trivia' : content})
+
+
+def about(request):
+    return render(request, 'trivia/about.html')
+
+def categories(request):
+    return render(request, 'trivia/categories.html')
