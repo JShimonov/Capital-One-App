8b774052965a8ff7e35f28521f613351cba47682
JShimonov <jshimonov@gmail.com>
2019-10-25 22:49:23 -0400

    added CSS into home page

--- trivia/views.py
+++ trivia/views.py
@@ -1,37 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 import requests
-import re
-
-'''
-questions = [
-    {
-        "id":11510,
-        "title":"pair of dice, lost",
-        "clues_count":5
-    },
-    {
-        "id":11531,
-        "title":"mixed bag",
-        "clues_count":5
-    }
-]
-'''
-
 
 def home(request):
-    req = 'http://jservice.io/api/random?count=12'
+    req = 'http://jservice.io/api/random?count=8'
     response = requests.get(req)
     trivia_set = response.json()
     content = []
     for trivia in trivia_set:
-        dict = { 'id': trivia['id'], 'question' : trivia['question'], 'answer' : trivia['answer'], 'category' : trivia['category']['title']}
+        dict = {'id': trivia['id'], 'question' : trivia['question'], 'answer' : trivia['answer'], 'category' : trivia['category']['title']}
         content.append(dict)
     return render(request, 'trivia/home.html', {'trivia':content})
+
+def categories(request):
+    req = 'http://jservice.io/api/categories?100'
+    response = requests.get(req)
+    category_set = response.json()
+    content = []
+    for category in category_set:
+        dict = {'id': category['id'], 'title': category['title'], 'clues_count': category['clues_count']}
+        content.append(dict)
+    return render(request, 'trivia/categories.html', {'categories':content})
 
 
 def about(request):
     return render(request, 'trivia/about.html')
-
-def categories(request):
-    return render(request, 'trivia/categories.html')
